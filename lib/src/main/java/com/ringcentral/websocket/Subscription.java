1ea5d30e1db4ef3c896c96f165450da25b03c444
Tyler Long <tyler4long@gmail.com>
2023-04-15 16:59:39 -0700

    Create subscription

--- lib/src/main/java/com/ringcentral/websocket/Subscription.java
+++ lib/src/main/java/com/ringcentral/websocket/Subscription.java
@@ -7,39 +7,77 @@ import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
+import java.util.UUID;
+
+class RequestHeaders {
+    public String type;
+    public String messageId;
+    public String method;
+    public String path;
+}
+
+class SubscriptionRequestBody
+{
+    public SubscriptionRequestBodyDeliveryMode deliveryMode;
+    public String[] eventFilters;
+}
+class SubscriptionRequestBodyDeliveryMode
+{
+    public String transportType;
+}
+
+class MyWebSocketClient extends WebSocketClient {
+
+    private String[] _eventFilters;
+    private EventListener _eventListener;
+
+    public MyWebSocketClient(URI serverUri, String[] eventFilters, EventListener eventListener) {
+        super(serverUri);
+        this._eventFilters = eventFilters;
+        this._eventListener = eventListener;
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshakedata) {
+        System.out.println("onOpen");
+        SubscriptionRequestBody requestBody = new SubscriptionRequestBody();
+        requestBody.deliveryMode = new SubscriptionRequestBodyDeliveryMode();
+        requestBody.deliveryMode.transportType = "WebSocket";
+        requestBody.eventFilters = this._eventFilters;
+
+        RequestHeaders requestHeaders = new RequestHeaders();
+        requestHeaders.method = "POST";
+        requestHeaders.path = "/restapi/v1.0/subscription";
+        requestHeaders.type = "ClientRequest";
+        requestHeaders.messageId = UUID.randomUUID().toString();
+
+        Object[] array = new Object[2];
+        array[0] = requestHeaders;
+        array[1] = requestBody;
+
+        this.send(Utils.gson.toJson(array));
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("onMessage: " + message);
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("onClose: " + code + " " + reason);
+    }
+
+    @Override
+    public void onError(Exception ex) {
+        System.out.println("onError");
+    }
+}
 
 public class Subscription {
     private RestClient _restClient;
     private String[] _eventFilters;
     private EventListener _eventListener;
-
-    class MyWebSocketClient extends WebSocketClient {
-
-        public MyWebSocketClient(URI serverUri) {
-            super(serverUri);
-        }
-
-        @Override
-        public void onOpen(ServerHandshake handshakedata) {
-            System.out.println("onOpen");
-        }
-
-        @Override
-        public void onMessage(String message) {
-            System.out.println("onMessage: " + message);
-        }
-
-        @Override
-        public void onClose(int code, String reason, boolean remote) {
-            System.out.println("onClose: " + code + " " + reason);
-        }
-
-        @Override
-        public void onError(Exception ex) {
-            System.out.println("onError");
-        }
-    }
-
 
     public Subscription(RestClient restClient, String[] eventFilters, EventListener eventListener) {
         this._restClient = restClient;
@@ -51,7 +89,7 @@ public class Subscription {
         ResponseBody responseBody = this._restClient.post("/restapi/oauth/wstoken");
         WsToken wsToken = Utils.gson.fromJson(responseBody.string(), WsToken.class);
         String wsUri = wsToken.uri + "?access_token=" + wsToken.ws_access_token;
-        MyWebSocketClient myClient = new MyWebSocketClient(URI.create(wsUri));
+        MyWebSocketClient myClient = new MyWebSocketClient(URI.create(wsUri), _eventFilters, _eventListener);
         myClient.connect();
     }
 }
